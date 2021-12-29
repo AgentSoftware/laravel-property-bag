9de03b2a7adfa78d0937c5ef3e2ca650c3e6fe36
Jim Cretney <jacretney@gmail.com>
2021-12-29 14:55:29 +0000

    Update test to use short class name

--- tests/Unit/Helpers/NameResolverTest.php
+++ tests/Unit/Helpers/NameResolverTest.php
@@ -5,13 +5,25 @@ namespace LaravelPropertyBag\tests\Unit\Helpers;
 use LaravelPropertyBag\Helpers\NameResolver;
 use LaravelPropertyBag\tests\Classes\User;
 use LaravelPropertyBag\tests\TestCase;
+use ReflectionClass;
 
 class NameResolverTest extends TestCase
 {
+    /** @var string $shortClassName */
+    private $shortClassName;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $reflection = new ReflectionClass(new User());
+        $this->shortClassName = $reflection->getShortName();
+    }
+
     public function test_can_make_config_filename_if_no_user_namespace_config_set(): void
     {
-        $namespace = NameResolver::makeConfigFileName(User::class);
-        $this->assertEquals('App\Settings\LaravelPropertyBag\tests\Classes\UserSettings', $namespace);
+        $namespace = NameResolver::makeConfigFileName($this->shortClassName);
+        $this->assertEquals('App\Settings\UserSettings', $namespace);
     }
 
     public function test_can_make_config_filename_if_user_namespace_config_set(): void
@@ -20,8 +32,8 @@ class NameResolverTest extends TestCase
             'property_bag.namespace' => 'MyApp\\Settings'
         ]);
 
-        $namespace = NameResolver::makeConfigFileName(User::class);
-        $this->assertEquals('MyApp\Settings\LaravelPropertyBag\tests\Classes\UserSettings', $namespace);
+        $namespace = NameResolver::makeConfigFileName($this->shortClassName);
+        $this->assertEquals('MyApp\Settings\UserSettings', $namespace);
     }
 
     public function test_can_make_rules_filename_if_no_user_namespace_config_set(): void
